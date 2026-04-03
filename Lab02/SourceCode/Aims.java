17468ae71273077984d492bbc84648b52a2dc878
lumanhthanhvblcvn@gmail.com <lumanhthanhvblcvn@gmail.com>
2026-04-04 00:42:34 +0700

    Submit Lab02

--- Lab02/SourceCode/Aims.java
+++ Lab02/SourceCode/Aims.java
@@ -19,5 +19,17 @@ public class Aims {
         // In ra tổng chi phí của các mặt hàng trong giỏ
         System.out.println("Total Cost is: ");
         System.out.println(anOrder.totalCost());
+
+        // ---------------------------------------------------
+        // PHẦN CODE CHO MỤC 11: KIỂM TRA TÍNH NĂNG XÓA
+        // ---------------------------------------------------
+        System.out.println("\n--- Bắt đầu test xóa đĩa ---");
+
+        // Gọi hàm xóa đĩa dvd2 (Star Wars) khỏi giỏ hàng
+        anOrder.removeDigitalVideoDisc(dvd2);
+
+        // In lại tổng tiền xem đã trừ đi giá trị của đĩa bị xóa chưa
+        System.out.println("Total Cost after removing is: ");
+        System.out.println(anOrder.totalCost());
     }
 }
