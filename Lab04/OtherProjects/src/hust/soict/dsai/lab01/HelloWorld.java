35fb322ea97f2604bd9da3c021671ea7c1eb1460
Launthh <lumanhthanhvblcvn@gmail.com>
2026-05-23 06:19:11 +0700

    .

--- Lab04/OtherProjects/src/hust/soict/dsai/lab01/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.dsai.lab01;
-
-public class HelloWorld {
-    public static void main(String args[]){
-        System.out.println("Xin Chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
